af5a83c459f1d4c23bf422cea21cb5122d0f818d
Gianfranco Tomaino <gtomaino@hotmail.com>
2025-05-02 23:13:03 +0200

    Versiones de 300 epochs

--- Skin_Cancer_VGG19_01May2025_Modelo_F_LORCA.py
+++ Skin_Cancer_VGG19_01May2025_Modelo_F_LORCA.py
@@ -133,7 +133,7 @@ if __name__ == '__main__':
     print(f"Using device: {device}")
 
     # Define the number of epochs for training
-    num_epochs = 200
+    num_epochs = 300
 
     # Let's get a model summary
     print()
