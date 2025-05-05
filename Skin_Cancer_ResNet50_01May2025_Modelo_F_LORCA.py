c5c14d2e92f06b52229473b7157c3dd7e0fc132d
Gianfranco Tomaino <gtomaino@hotmail.com>
2025-05-05 16:23:20 +0200

    Ejecucion sin DA 05052025

--- Skin_Cancer_ResNet50_01May2025_Modelo_F_LORCA.py
+++ Skin_Cancer_ResNet50_01May2025_Modelo_F_LORCA.py
@@ -31,15 +31,15 @@ if __name__ == '__main__':
     # Create a transform function for Skin Cancer dataset
     transform_train = transforms.Compose([
         transforms.Resize((224, 224)),
-        transforms.RandomResizedCrop(224),
-        transforms.RandomHorizontalFlip(),
+        #transforms.RandomResizedCrop(224),
+        #transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
 
     transform_test = transforms.Compose([
         transforms.Resize((224, 224)),
-        transforms.CenterCrop((224, 224)),
+        #transforms.CenterCrop((224, 224)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
